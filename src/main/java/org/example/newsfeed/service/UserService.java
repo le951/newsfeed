ae4efe6648129b80f3be0ed01b42f72022d4e522
believeme <cain1227@naver.com>
2025-04-08 11:09:38 +0900

    Update and rename UserServiceImpl.java to UserService.java

--- src/main/java/org/example/newsfeed/service/UserService.java
+++ src/main/java/org/example/newsfeed/service/UserService.java
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserService {
 
 	private final UserRepository userRepository;
 
