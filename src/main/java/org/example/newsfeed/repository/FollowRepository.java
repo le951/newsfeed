2e21abc9c61585e5e09233942a96cc2e924efaed
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-08 14:31:06 +0900

    init

--- src/main/java/org/example/newsfeed/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed/repository/FollowRepository.java
@@ -2,8 +2,7 @@ package org.example.newsfeed.repository;
 
 import org.example.newsfeed.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 }
--- src/main/java/org/example/newsfeed/repository/UserRepository.java
+++ src/main/java/org/example/newsfeed/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package org.example.newsfeed.repository;
 
 import java.util.Optional;
-
 import org.example.newsfeed.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByNickname(String nickname);
 	Optional<User> findByEmail(String email);
