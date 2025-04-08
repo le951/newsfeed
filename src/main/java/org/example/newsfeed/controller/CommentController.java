ebad8c45fdee5ab8457a7244f2d771d97c36aed9
se0yu <pr924133@gmail.com>
2025-04-08 20:33:01 +0900

    feat: 댓글 수정 기능 추가 style: 댓글 수정 pathvariable 변수명 변경 id -> newsfeedsId로 변경 CommentRepository의 findByBoardId -> findAllbyBoardId

--- src/main/java/org/example/newsfeed/controller/CommentController.java
+++ src/main/java/org/example/newsfeed/controller/CommentController.java
@@ -1,17 +1,15 @@
 package org.example.newsfeed.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeed.dto.comment.CommentRequestDto;
-import org.example.newsfeed.dto.comment.CommentResponseDto;
 import org.example.newsfeed.service.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/newsfeeds/{newsfeedsId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
@@ -19,28 +17,44 @@ public class CommentController {
 
     //댓글 작성 기능
     @PostMapping("/newsfeeds/{newsfeedsId}/comments")
-    public ResponseEntity<Void> saveComment(
+    public ResponseEntity<String> saveComment(
             @PathVariable Long newsfeedsId,
-//            HttpServletRequest httpServletRequest,
+            //            HttpServletRequest httpServletRequest,
             @Valid @RequestBody CommentRequestDto requsetDto
-    ){
+    ) {
 
         //현재 userId 는 테스트용도 - 후에 수정할 예정
-        /*
-        테스트 당시 board 기능이 구현되지 않아 console에서 하단 쿼리문 실행 후 테스트함
+         /*
+         테스트 당시 board 기능이 구현되지 않아 console에서 하단 쿼리문 실행 후 테스트함
 
-        INSERT INTO boards(title, contents,user_id,created_at)
-        VALUES('타이틀','내용',1,'2025-04-08');
+         INSERT INTO boards(title, contents,user_id,created_at)
+         VALUES('타이틀','내용',1,'2025-04-08');
 
-        localhost:8080/newsfeeds/1/comments
+         localhost:8080/newsfeeds/1/comments
 
-        {
-            "comments" : "댓글3"
-        }
+         {
+         "comments" : "댓글3"
+         }
 
-         */
-        CommentResponseDto responseDto = commentService.saveComment(newsfeedsId,1L,requsetDto);
+          */
+        commentService.saveComment(newsfeedsId, 1L, requsetDto);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>("댓글 작성 완료", HttpStatus.CREATED);
+    }
+
+    //댓글 수정 기능
+    //Response 메세지는 임시 - 후에 논의 후 수정
+    @PatchMapping("/comments/{commentsId}")
+    public ResponseEntity<String> updateComment(
+            @PathVariable Long id,
+            @PathVariable Long commmetsId,
+            //HttpServletRequest httpServletRequest,
+            @Valid @RequestBody CommentRequestDto requestDto
+    ) {
+        //LoginResponseDto loginUser = (LoginResponseDto) httpServletRequest.getSession().getAttribute(Const.LOGIN_USER);
+        commentService.updateComment(id, 1L, requestDto);
+        commentService.updateComment(commmetsId, 1L, requestDto);
+
+        return new ResponseEntity<>("댓글 수정 완료", HttpStatus.OK);
     }
 }
