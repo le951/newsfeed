e34de80a48bc5ef5bd586541ab08ef2524da8bd4
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-08 13:59:57 +0900

    feat: BoardController class 게시물 수정 기능 구현

--- src/main/java/org/example/newsfeed/controller/BoardController.java
+++ src/main/java/org/example/newsfeed/controller/BoardController.java
@@ -8,6 +8,8 @@ import org.example.newsfeed.dto.board.BoardResponseDto;
 import org.example.newsfeed.service.BoardService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,24 @@ public class BoardController {
     BoardResponseDto boardResponseDto = boardService.saveBoard(loginUser.getId(), requestDto);
 
     return new ResponseEntity<>(boardResponseDto, HttpStatus.CREATED);
+  }
+
+  // 게시물 수정
+  @PatchMapping("/{boardId}")
+  public ResponseEntity<BoardResponseDto> updateBoard(
+      @PathVariable Long boardId,
+      @RequestBody BoardRequestDto requestDto,
+      @SessionAttribute(name = Const.LOGIN_USER, required = false) LoginResposneDto loginUser
+  ){
+
+    BoardResponseDto boardResponseDto =
+        boardService.updateBoard(
+            loginUser.getId(),
+            boardId,
+            requestDto
+        );
+
+    return new ResponseEntity<>(boardResponseDto, HttpStatus.OK);
   }
 
 
