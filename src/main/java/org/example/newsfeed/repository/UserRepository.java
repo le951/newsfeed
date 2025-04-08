c0f5fcd691d00d36c9a8bcfec53110331d23d1ae
le951 <ihyeongjin043@gmail.com>
2025-04-08 13:29:51 +0900

    auth 테스트용 Controller / http Request 파일 추가 (signUp / Login)

--- src/main/java/org/example/newsfeed/repository/UserRepository.java
+++ src/main/java/org/example/newsfeed/repository/UserRepository.java
@@ -4,6 +4,9 @@ import org.example.newsfeed.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Email은 Unique. List<User> 가 아닌 User
+    User findByEmail(String email);
 }
