6a16381b4fee11c745e3c9cdf8b111e11241a29d
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-08 14:44:32 +0900

    init

--- src/main/java/org/example/newsfeed/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeed/repository/FollowRepository.java
@@ -2,7 +2,8 @@ package org.example.newsfeed.repository;
 
 import org.example.newsfeed.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 }
