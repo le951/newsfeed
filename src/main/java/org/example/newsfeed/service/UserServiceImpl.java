3644f476056ebcb4c24481877cf7e01abc7e81c8
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-04-08 12:26:16 +0900

    Feat: 유저 탈퇴 기능

--- src/main/java/org/example/newsfeed/service/UserServiceImpl.java
+++ src/main/java/org/example/newsfeed/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package org.example.newsfeed.service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.example.newsfeed.dto.user.SignUpRequestDto;
 import org.example.newsfeed.dto.user.SignUpResponseDto;
 import org.example.newsfeed.dto.user.UserResponseDto;
+import org.example.newsfeed.entity.DeletedUser;
 import org.example.newsfeed.entity.User;
+import org.example.newsfeed.repository.DeletedUserRepository;
 import org.example.newsfeed.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,36 +16,56 @@ import org.springframework.web.server.ResponseStatusException;
 
 import lombok.RequiredArgsConstructor;
 
+import static java.time.LocalTime.now;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final DeletedUserRepository deletedUserRepository;
 
-	@Override
-	public SignUpResponseDto signUp(SignUpRequestDto dto) {
-		User user = new User(dto.getNickname(), dto.getEmail(), dto.getPassword(), dto.getBirth());
-		User savedUser = userRepository.save(user);
-		return new SignUpResponseDto(savedUser.getNickname(), savedUser.getEmail());
-	}
+    @Override
+    public SignUpResponseDto signUp(SignUpRequestDto dto) {
+        User user = new User(dto.getNickname(), dto.getEmail(), dto.getPassword(), dto.getBirth());
+        User savedUser = userRepository.save(user);
+        return new SignUpResponseDto(savedUser.getNickname(), savedUser.getEmail());
+    }
 
-	@Override
-	public UserResponseDto findByNickname(String nickname) {
-		Optional<User> optionalUser = userRepository.findByNickname(nickname);
-		if(optionalUser.isEmpty()) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, nickname + "은 존재하지 않는 회원입니다.");
-		}
-		User findUser = optionalUser.get();
-		return new UserResponseDto(findUser.getNickname(), findUser.getEmail());
-	}
+    @Override
+    public UserResponseDto findByNickname(String nickname) {
+        Optional<User> optionalUser = userRepository.findByNickname(nickname);
+        if(optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, nickname + "은 존재하지 않는 회원입니다.");
+        }
+        User findUser = optionalUser.get();
+        return new UserResponseDto(findUser.getNickname(), findUser.getEmail());
+    }
 
-	@Override
-	public UserResponseDto findByEmail(String email) {
-		Optional<User> optionalUser = userRepository.findByEmail(email);
-		if(optionalUser.isEmpty()) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, email + "은 존재하지 않는 회원입니다.");
-		}
-		User findUser = optionalUser.get();
-		return new UserResponseDto(findUser.getNickname(), findUser.getEmail());
-	}
+    @Override
+    public UserResponseDto findByEmail(String email) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if(optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, email + "은 존재하지 않는 회원입니다.");
+        }
+        User findUser = optionalUser.get();
+        return new UserResponseDto(findUser.getNickname(), findUser.getEmail());
+    }
+
+    @Override
+    public void delete(Long id, String password) {
+
+        User findUser = userRepository.findByIdOrElseThrow(id);
+
+        String userPassword = findUser.getPassword();
+
+        if (!userPassword.equals(password)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호");
+        }
+
+        DeletedUser deletedUser = new DeletedUser(findUser, LocalDateTime.now());
+        deletedUserRepository.save(deletedUser);
+
+        userRepository.delete(findUser);
+    }
 }
