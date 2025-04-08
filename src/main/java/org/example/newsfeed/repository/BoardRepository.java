80fab88a92c0b0f94f30c911573aa57da844697e
songgaeun <rkdmsdmqek1128@gmail.com>
2025-04-08 14:00:33 +0900

    feat: BoardRepository 게시물 찾아오기

--- src/main/java/org/example/newsfeed/repository/BoardRepository.java
+++ src/main/java/org/example/newsfeed/repository/BoardRepository.java
@@ -1,9 +1,24 @@
 package org.example.newsfeed.repository;
 
+import java.util.Optional;
 import org.example.newsfeed.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+  Optional<Board> findByUserIdAndId(Long userId, Long id);
+
+  default Board findByUserIdAndIdOrElseThrow(Long userId, Long boardId){
+    return findByUserIdAndId(userId,boardId)
+        .orElseThrow(()->
+            new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Does not exist id = " + boardId
+            )
+        );
+  }
 }
