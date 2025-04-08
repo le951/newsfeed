24d64e0a3a0afdabdd426a24959d6b7bb20e9ff1
pleasebelieveme <cain1227@naver.com>
2025-04-08 12:23:24 +0900

    refactor: @Repository 삭제

--- src/main/java/org/example/newsfeed/repository/BoardRepository.java
+++ src/main/java/org/example/newsfeed/repository/BoardRepository.java
@@ -4,6 +4,5 @@ import org.example.newsfeed.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
