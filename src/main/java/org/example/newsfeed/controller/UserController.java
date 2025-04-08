57f5f828185e291ed343fa058e9544ca05d80f48
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-04-08 12:26:16 +0900

    Feat: 유저 탈퇴 기능

--- src/main/java/org/example/newsfeed/controller/UserController.java
+++ src/main/java/org/example/newsfeed/controller/UserController.java
@@ -1,15 +1,20 @@
 package org.example.newsfeed.controller;
 
-import org.example.newsfeed.dto.user.DeleteUserRequestDto;
 import org.example.newsfeed.dto.user.SignUpRequestDto;
 import org.example.newsfeed.dto.user.SignUpResponseDto;
 import org.example.newsfeed.dto.user.UpdateUserRequestDto;
 import org.example.newsfeed.dto.user.UserResponseDto;
-import org.example.newsfeed.dto.user.UpdatePasswordRequestDto;
 import org.example.newsfeed.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,17 +46,9 @@ public class UserController {
 		}
 	}
 
-	@PatchMapping("/{id}")
-	public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UpdatePasswordRequestDto dto) {
-		userService.updatePassword(id, dto.getOldPassword(), dto.getNewPassword());
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-
-	@DeleteMapping("/{id}")
-	private ResponseEntity<Void> deleteUser(@PathVariable Long id, @RequestBody DeleteUserRequestDto dto) {
-
-		userService.delete(id, dto.getPassword());
-
+	@PatchMapping
+	public ResponseEntity<Void> updatePassword(@RequestBody UpdateUserRequestDto dto) {
+		userService.updatePassword(dto.getNickname(), dto.getOldPassword(), dto.getNewPassword());
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
