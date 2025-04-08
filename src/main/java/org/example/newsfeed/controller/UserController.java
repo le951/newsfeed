f044921e7170993aee41042957ad18cbfe729b78
pleasebelieveme <cain1227@naver.com>
2025-04-08 13:31:48 +0900

    refactor: 유저 CRUD 로직 완성

--- src/main/java/org/example/newsfeed/controller/UserController.java
+++ src/main/java/org/example/newsfeed/controller/UserController.java
@@ -4,6 +4,7 @@ import org.example.newsfeed.dto.user.SignUpRequestDto;
 import org.example.newsfeed.dto.user.SignUpResponseDto;
 import org.example.newsfeed.dto.user.UpdateUserRequestDto;
 import org.example.newsfeed.dto.user.UserResponseDto;
+import org.example.newsfeed.dto.user.UpdatePasswordRequestDto;
 import org.example.newsfeed.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,9 +47,9 @@ public class UserController {
 		}
 	}
 
-	@PatchMapping
-	public ResponseEntity<Void> updatePassword(@RequestBody UpdateUserRequestDto dto) {
-		userService.updatePassword(dto.getNickname(), dto.getOldPassword(), dto.getNewPassword());
+	@PatchMapping("/{id}")
+	public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UpdatePasswordRequestDto dto) {
+		userService.updatePassword(id, dto.getOldPassword(), dto.getNewPassword());
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
