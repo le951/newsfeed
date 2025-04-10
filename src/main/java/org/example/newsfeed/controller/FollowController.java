c009d8dd848465c10628014ba13e7e8741f8ebb9
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-04-10 12:26:50 +0900

    Refactor: Follow 리팩토링 & 주석 추가

--- src/main/java/org/example/newsfeed/controller/FollowController.java
+++ src/main/java/org/example/newsfeed/controller/FollowController.java
@@ -22,37 +22,46 @@ public class FollowController {
     private final UserService userService;
     private final FollowService followService;
 
+    // 팔로우
     @PostMapping
     public ResponseEntity<FollowResponseDto> followUser(@Valid @RequestBody UserRequestDto requestDto, HttpServletRequest servletRequest) {
 
         String email = requestDto.getEmail();
         String nickname = requestDto.getNickname();
 
+        // jwt 토큰에서 userId 정보를 가져옴
         Long userId = (Long) servletRequest.getAttribute("userId");
 
-        if (userId == null) {
-            throw new CustomException(ErrorCode.USER_NOT_FOUND);
-        }
+        // userId가 NULL 값이 아닌지 확인
+        checkedLogin(userId);
 
+        // 이메일과 닉네임으로 상대방(팔로우 받는 사람)의 유저 정보를 가져옴
         UserResponseDto findUser = checkEmailOrNickname(nickname, email);
 
         // 자기 자신을 팔로우 불가
         checkNotSelfAction(findUser.getId(), userId);
 
+        // 팔로우
         FollowResponseDto follow = followService.follow(findUser.getId(), userId);
 
         return new ResponseEntity<>(follow, HttpStatus.OK);
 
     }
 
+    // 언팔로우
     @DeleteMapping
     public ResponseEntity<String> unfollowUser(@Valid @RequestBody UserRequestDto requestDto, HttpServletRequest servletRequest) {
 
         String email = requestDto.getEmail();
         String nickname = requestDto.getNickname();
 
+        // jwt 토큰에서 userId 정보를 가져옴
         Long userId = (Long) servletRequest.getAttribute("userId");
 
+        // userId가 NULL 값이 아닌지 확인
+        checkedLogin(userId);
+
+        // 이메일과 닉네임으로 상대방(언팔로우 받는 사람)의 유저 정보를 가져옴
         UserResponseDto findUser = checkEmailOrNickname(nickname, email);
 
         // 본인 언팔 불가
@@ -62,14 +71,20 @@ public class FollowController {
         return new ResponseEntity<>("언팔로우", HttpStatus.OK);
     }
 
+    // 일단 맞팔을 확인하고 팔로워와 팔로윙을 하는 사람의 수를 체크하는 기능..
     @GetMapping
     public ResponseEntity<String> followBack(@Valid @RequestBody UserRequestDto requestDto, HttpServletRequest servletRequest) {
 
         String email = requestDto.getEmail();
         String nickname = requestDto.getNickname();
 
+        // jwt 토큰에서 userId 정보를 가져옴
         Long userId = (Long) servletRequest.getAttribute("userId");
 
+        // userId가 NULL 값이 아닌지 확인
+        checkedLogin(userId);
+
+        // 이메일과 닉네임으로 상대방(언팔로우 받는 사람)의 유저 정보를 가져옴
         UserResponseDto findUser = checkEmailOrNickname(nickname, email);
 
         String isFollowBack = followService.checkFollowBack(findUser.getId(), userId);
@@ -77,6 +92,7 @@ public class FollowController {
         return new ResponseEntity<>(isFollowBack, HttpStatus.OK);
     }
 
+    // 닉네임과 이메일 값중 존재하는 값으로 유저를 조회후 반환
     public UserResponseDto checkEmailOrNickname(String nickname, String email) {
         if (email != null) {
             return userService.findByEmail(email);
@@ -87,9 +103,17 @@ public class FollowController {
         }
     }
 
+    // 본인 스스로에게 액션(팔로우, 언팔로우)하면 예외처리
     public void checkNotSelfAction(Long toUserId, Long fromUserId) {
         if (toUserId.equals(fromUserId)) {
             throw new CustomException(ErrorCode.ACTION_SELF_ACCOUNT);
+        }
+    }
+
+    // 현재 userId값이 Null 값인지 확인후 Null 값이면 예외 처리
+    public void checkedLogin(Long userId) {
+        if (userId == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
         }
     }
 }
--- src/main/java/org/example/newsfeed/dto/follow/FollowResponseDto.java
+++ src/main/java/org/example/newsfeed/dto/follow/FollowResponseDto.java
@@ -7,10 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class FollowResponseDto {
 
+    // 팔로우 받는 사람
     private final Long toUserId;
 
+    // 팔로우 요청을 하는 사람
     private final Long fromUserId;
 
     private final String message;
-
 }
